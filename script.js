3399717c3de53145e5b216c35c4fd2f72d4e164f
BrookeAFoote <bfoote@tulane.edu>
2018-02-06 16:57:21 -0800

    fixing input

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 
 $(document).ready(function() {
 //click on table
+var tableEl;
     $(".available").on("click", function(){
         //--reveals form
         $(this).css("background", "#ADAAAA");
         $(".hidden").slideToggle("show");
+        tableEl = $(this)
+        var tableNum = tableEl.text();
+        $("#tableNumber").text(tableNum);
+        addHoverData()
         //animations
      })//onclick
 
+     function addHoverData() {
+        var name = $("#name").val();
+        var partySize = $("#guests").val();
+        tableEl.append('<div class="hidden"><div>Guest Name:' + name + '</div><div>Size of Party:' + partySize + '</div></div>'
+        );
+     }
+
 // click on save button
-     $("button").on("click", function(){
+     $("button").on("click", function(event){
          event.preventDefault();
         //--hides form
         $(".hidden").slideToggle("show");
         //animations
         //--changes table color
-        $(".table").addClass(".reserved");
-        $(".table").removeClass(".available");
+        tableEl.addClass(".reserved");
+        tableEl.removeClass(".available");
      })
-//store input
-var nameInput = $("#name").val
-var phoneInput = $("#phone").val
-var guestsInput = $("#guests").val
 
-//dislay input on hover
+
+
+//mouse over
+
+//- show data
+//mouse out
+//- hide data
 
 
 
